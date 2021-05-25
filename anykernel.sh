0b24991a974742e614c48112927be4524728342f
johnmart19 <johnivan19999@gmail.com>
2021-05-25 16:56:48 +0300

    Alioth: AnyKernel3 Bringup

--- anykernel.sh
+++ anykernel.sh
@@ -4,40 +4,28 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Redline Kernel by johnmart19
+kernel.string=
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=ginkgo
-device.name2=willow
-device.name3=
-device.name4=
-device.name5=
-supported.versions=10.0-11.0
-supported.patchlevels=
-'; } # end properties
+device.name1=alioth
+device.name2=aliothin
+'; }
 
 # shell variables
-block=/dev/block/bootdevice/by-name/boot;
+block=/dev/block/bootdevice/by-name/boot_a;
 is_slot_device=0;
 ramdisk_compression=auto;
-
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-
-## AnyKernel file attributes
-# set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-
-
 ## AnyKernel install
 dump_boot;
+
 write_boot;
 ## end install
 
